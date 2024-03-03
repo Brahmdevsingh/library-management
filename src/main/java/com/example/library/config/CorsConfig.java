8a67c572b26be518c5d658aca2f51a31ac8664f6
Brahmdevsingh <65753877+Brahmdevsingh@users.noreply.github.com>
2024-03-03 21:10:16 +0530

    Eleventh Commit

--- src/main/java/com/example/library/config/CorsConfig.java
+++ src/main/java/com/example/library/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		 registry.addMapping("/v3/api-docs")
-         .allowedOrigins("*")
+         .allowedOrigins("https://library-management-production-3216.up.railway.app")
          .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
          .allowedHeaders("*")
          .exposedHeaders("Authorization")
